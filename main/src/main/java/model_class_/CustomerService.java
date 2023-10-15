638a16ef5d798bbb61cf5a3f1d8579baa5efd9d1
Konf1ic <114355460+Konf1ic@users.noreply.github.com>
2023-10-15 18:29:30 +0700

    AppQLSP

---
+++ main/src/main/java/model_class_/CustomerService.java
@@ -0,0 +1,15 @@
+package model_class_;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
